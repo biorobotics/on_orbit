525ce1a982b92c78bfa1ba36c3d303fbaf5aa074
Benjamin Spin <149523012+BenSpin@users.noreply.github.com>
2024-09-16 09:00:01 -0400

    Revert "Holodeck integration/insertion pipeline for demo (#3)"
    This reverts commit 2cea9d495a1852177b824da3767f72b207106904.

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/medusal/suri_ws/src/on_orbit
-# Build directory: /home/medusal/suri_ws/src/on_orbit/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;ur_state_machine".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "on_orbit"
-PROJECT_SPACE_DIR = "/home/medusal/suri_ws/src/on_orbit/build/devel"
-PROJECT_VERSION = "0.0.0"
